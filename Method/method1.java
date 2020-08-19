53aad76579f4572f13c82ee6cc5a4cd64ca4cd26
joker77z <50583262+joker77z@users.noreply.github.com>
2020-08-19 15:36:45 +0900

    인자, 매개변수

---
+++ Method/method1.java
@@ -0,0 +1,17 @@
+
+public class method1 {
+
+	public static void main(String[] args) {
+			   //인자, argument
+		print1("aaa", "-");
+	}
+
+									//매개변수, parameter
+	public static void print1(String text, String delemeter) {
+		System.out.println(text);
+		System.out.println(delemeter);
+		System.out.println("b");
+		System.out.println("b");
+	}
+
+}
