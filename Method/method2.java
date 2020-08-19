f1ff57844880caf6255cca3ed2dcb1ba0304f8ce
joker77z <50583262+joker77z@users.noreply.github.com>
2020-08-19 16:03:24 +0900

    return

---
+++ Method/method2.java
@@ -0,0 +1,16 @@
+
+public class method2 {
+
+	public static void main(String[] args) {
+		System.out.println(print1());
+		System.out.println(print2());
+	}
+
+	public static String print1() {
+		return "a";
+	}
+
+	public static int print2() {
+		return 1;
+	}
+}
