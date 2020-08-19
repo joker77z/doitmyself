c0ef29fcbda38655493daa0a1ec6cf992098733b
joker77z <50583262+joker77z@users.noreply.github.com>
2020-08-19 16:13:14 +0900

    메소드의 응용(파일로 만들기)

--- Method/method2.java
+++ Method/method2.java
@@ -1,16 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class method2 {
 
-	public static void main(String[] args) {
-		System.out.println(print1());
-		System.out.println(print2());
+	public static void main(String[] args) throws IOException {
+		FileWriter fw = new FileWriter("abc.txt");
+		fw.write(print1("test", "-"));
+		fw.close();
+//		아래는 없는 코드지만 이런식으로 사용이 가능하다
+//		Email.send("joker77z@naver.com", "title", print1("안녕하세요", "-");
 	}
 
-	public static String print1() {
-		return "a";
-	}
-
-	public static int print2() {
-		return 1;
+	public static String print1(String text, String delimiter) {
+		String out = "";
+		out = out + text + "\n";
+		out = out + delimiter + "\n";
+		return out;
 	}
 }
