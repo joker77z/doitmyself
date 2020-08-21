e7bdd915d26d5818249c3b817fd3bfa8d861e72c
joker77z <50583262+joker77z@users.noreply.github.com>
2020-08-21 17:58:36 +0900

    interface

--- inheritance/inheritance62.java
+++ inheritance/inheritance62.java
@@ -1,6 +1,6 @@
-class calculator3 {
+class calculator6 {
 	int a, b;
-	public calculator3(int a, int b) {
+	public calculator6(int a, int b) {
 		this.a = a;
 		this.b = b;
 	}
@@ -9,19 +9,20 @@ class calculator3 {
 	}
 }
 
-class calculator4 extends calculator3{
-	public calculator4(int a, int b) {
+class calculator7 extends calculator6{
+	public calculator7(int a, int b) {
 		super(a,b);
 	}
 	public int minus() {
 		return this.a-b;
 	}
 }
-public class inheritance61 {
+public class inheritance62 {
 	public static void main(String[] args) {
-		calculator3 c1 = new calculator3(1, 2);
+		calculator6 c1 = new calculator6(1, 2);
 		System.out.println(c1.sum());
-		calculator4 c2 = new calculator4(2, 1);
+		
+		calculator7 c2 = new calculator7(2, 1);
 		System.out.println(c2.minus());
 	}
 }
